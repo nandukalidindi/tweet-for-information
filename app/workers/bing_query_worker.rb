e2e35b9d598059c9a8f81a44b5dca07dd5316067
nandukalidindi <nandukalidindi@gmail.com>
2017-05-02 23:11:47 -0400

    Remove dump and bing query worker stub

---
+++ app/workers/bing_query_worker.rb
@@ -0,0 +1,12 @@
+class BingQueryWorker
+  include Sidekiq::Worker
+  sidekiq_options retry: false, :queue => :query
+
+  def perform(keyword_id)
+    user_keyword = UserKeyword.find(keyword_id)
+
+    if user_keyword.present?
+      
+    end
+  end
+end
